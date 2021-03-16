cacead6f735dc5886b2b2495ba71687c88434ba9
halki <s.halki0203@icloud.com>
2021-03-16 17:04:29 +0900

    post,photo_model_create

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_12_021201) do
+ActiveRecord::Schema.define(version: 2021_03_16_070744) do
+
+  create_table "photos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_photos_on_post_id"
+  end
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -23,4 +39,6 @@ ActiveRecord::Schema.define(version: 2021_03_12_021201) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "photos", "posts"
+  add_foreign_key "posts", "users"
 end
