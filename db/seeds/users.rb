04aec9019aec56c9926540d0199d65d6f11a89f0
halki <s.halki0203@icloud.com>
2021-03-22 22:11:31 +0900

    ignor追加、seedファイルの修正

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -2,9 +2,9 @@ puts 'Start inserting seed "users" ...'
 10.times do
   user = User.create(
       email: Faker::Internet.unique.email,
-      username: Faker::Internet.unique.user_name,
+      name: Faker::Internet.unique.user_name,
       password: 'password',
       password_confirmation: 'password'
       )
-  puts "\"#{user.username}\" has created!"
+  puts "\"#{user.name}\" has created!"
 end
