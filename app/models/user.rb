9c1b94757a8fc8ffd8185c344d38c9de436c1c5f
halki <s.halki0203@icloud.com>
2021-03-22 21:47:44 +0900

    postモデル作成、carrierwave導入
    ・postには外部機キーuser_idを設け、userモデルを参照できる様にしてます。

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,10 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true
   validates :name, presence: true
+
+  has_many :posts, dependent: :destroy
+
+  def own?(object)
+    id == object.user_id
+  end
 end
