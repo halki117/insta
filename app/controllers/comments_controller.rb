1f3efcbb77753daaa973a5b1a471f4da480e47af
halki <s.halki0203@icloud.com>
2021-03-28 22:38:26 +0900

    post,commentコントローラー実装

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,14 @@
+class CommentsController < ApplicationController
+  
+  def create
+    @comment = current_user.comments.new(comment_params)
+    @comment.save
+    redirect_back_or_to root_path
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :post_id)
+  end
+end
