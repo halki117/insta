03235d3415f68c8a98daf5cf004b3150a02879bc
halki <s.halki0203@icloud.com>
2021-03-31 15:21:18 +0900

    posts_controller, comment_viewファイル 一部修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -38,7 +38,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @comments = @post.comments.includes(:user).order(created_at: :desc)
-    @comment = Comments.new
+    @comment = Comment.new
   end
 
   def destroy
